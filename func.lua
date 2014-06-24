d441e0f26c147d9d00f959001ecb79e94e52a4bb
ywsample@gmail.com <ywsample@gmail.com>
2014-06-24 18:29:24 +0800

    when some function reference by more module,we than put than function to func moduel

--- func.lua
+++ func.lua
@@ -1,25 +1,32 @@
-local util = require "util";
-local bit = require "bit";
-local uuid = require "uuid";
-local list = require "lkfk.list";
-local base = require "lkfk.base";
 local const = require "lkfk.const";
-
-local concat = table.concat;
+local list = require "lkfk.list";
 local ipairs = ipairs;
-local pairs = pairs;
-local setmetatable = setmetatable;
-local error = error;
-local tonumber = tonumber;
 
-local ngxnow = ngx.now;
 local ngxlog = ngx.log;
-local ngxsleep = ngx.sleep;
-local floor = math.floor;
-local strlen = string.len
-local strsub = string.sub
+local ERR = ngx.ERR;
 
+local kfk_status = ngx.shared.kfk;
 
+local function kfk_status_add(key, value)
+    local size, err = kfk_status:incr(key, value);
+    if not size and err == "not found" then
+        local ok, err = kfk_status:add(key, 0);
+        if not ok and err ~= "exists" then
+            ngxlog(ERR, "shared dict error: ", err);
+        else
+            kfk_status:incr(key, value);
+        end
+    elseif not size then
+        ngxlog(ERR, "shared dict error: ", err);
+    end
+end
+
+local function kfk_status_set(key, value)
+    local size, err = kfk_status:set(key, value);
+    if not size then
+        ngxlog(ERR, "shared dict error: ", err);
+    end
+end
 
 local function kfk_topic_find(kfk, name)
 	local topics = kfk.kfk_topics;
@@ -31,56 +38,6 @@ local function kfk_topic_find(kfk, name)
 	return nil;
 end
 
-local function kfk_new_toppar(kfk_topic, partition)
-	local cf = kfk_topic.kfk.cf;
-	
-	local toppar = {
-        type = "toppar",
-		kfk_broker_toppar_link = list.LIST_ENTRY(),
-		kfk_topic = kfk_topic,
-		leader = nil,
-		partition = partition,
-		msgq = list.new("kfk_msg_link"),
-		fail_retry = 0,
-		last_send_time = 0
-	}
-	
-	return toppar;
-end
-
-local function kfk_new_topic(kfk, name, partitioner)
-	local kfk_topic = kfk_topic_find(kfk, name);
-	if kfk_topic then
-		return kfk_topic;
-	end
-	
-	kfk_topic = {
-        type = "topic",
-		kfk_topic_link = list.LIST_ENTRY(),
-        
-        kfk = kfk,
-        name = name,
-        state = const.KFK_TOPIC_INIT,
-        
-        partitioner = partitioner or kfk.cf.partitioner,
-        
-        kfk_toppars = util.new_tab(4, 0),
-        toppar_cnt = 0,
-
-        flag = 0
-	};
-	kfk_topic.toppar_ua = kfk_new_toppar(kfk_topic, const.KFK_PARTITION_UA);
-	
-	local topics = kfk.kfk_topics;
-	topics[#topics + 1] = kfk_topic;
-	return kfk_topic;
---	kfk.topic_cnt = kfk.topic_cnt + 1;
-	
-    --[[
-	ngx.log(ngx.DEBUG, "add topic: ", name);
-    --]]
-end
-
 local function kfk_toppar_get(kfk_topic, partition)
 	if partition >= 0 and partition < kfk_topic.toppar_cnt then
 		return kfk_topic.kfk_toppars[partition + 1];
@@ -89,75 +46,73 @@ local function kfk_toppar_get(kfk_topic, partition)
 	end
 end
 
-local function kfk_message_partitioner(kfk_topic, kfk_msg)
-	if kfk_topic.state == const.KFK_TOPIC_UNKNOWN then
-		return "topic unknown";
+local function kfk_broker_find_byid(kfk, nodeid)
+    local brokers = kfk.kfk_brokers;
+    local head = brokers.head;
+    local kfk_broker = head[brokers.key].next;
+    while kfk_broker ~= head do
+		if kfk_broker.nodeid == nodeid then
+			return kfk_broker;
+		end
+        kfk_broker = brokers:next(kfk_broker);
 	end
-	
-	local partition;
-	if kfk_topic.partition_cnt == 0 then
-		partition = const.KFK_PARTITION_UA;
-	else
-		partition = kfk_topic.partitioner(kfk_msg.key, kfk_topic.toppar_cnt);
-	end
-	
-	local kfk_toppar = kfk_toppar_get(kfk_topic, partition);
-	kfk_toppar.msgq:insert_tail(kfk_msg);
-    
-    --[[
-    ngx.log(ngx.DEBUG, "topic[", kfk_topic.name, ":", kfk_toppar.partition, 
-			"] have ", kfk_toppar.msgq.size, " messages");
-    --]]
+	return nil;
 end
 
-
-local function kfk_new_message(kfk, level, topic_name, key, str, prefix)
-	local cf = kfk.cf;
-	if level > cf.level then
-		return true;
+local function kfk_broker_ifup(kfk)
+    local brokers = kfk.kfk_brokers;
+    local head = brokers.head;
+    local kfk_broker = head[brokers.key].next;
+    while kfk_broker ~= head do
+		if kfk_broker.state == const.KFK_BROKER_UP then
+			return kfk_broker;
+		end
+        kfk_broker = brokers:next(kfk_broker);
 	end
-	
-	if kfk.msg_cnt + 1 > cf.queue_buffering_max_messages then
-        ngxlog(ngx.INFO, "nobufs");
-		return false, "nobufs";
-	end
-	
-    local prefix = prefix or uuid.unparse(uuid.generate("random"));
-	local kfk_msg = {
-		kfk_msg_link  = list.LIST_ENTRY(),
-		
-		ts_timeout = cf.message_timeout_ms + ngxnow(),
-		
-		level = level,
-		topic = topic_name,
-		key = key or prefix,
-		str = str
-	}
-	
-	local kfk_topic =  kfk_topic_find(kfk, topic_name);
-	if not kfk_topic then
-		ngxlog(ngx.ERR, "topic unknown: ", topic_name);
-		return false, "topic unknown";
-	end
-	
-	local err = kfk_message_partitioner(kfk_topic, kfk_msg);
-	if not err then
-		kfk.msg_cnt = kfk.msg_cnt + 1;
-		return true;
-	end
-	
-	ngxlog(ngx.ERR, "topic unknown: ", topic_name);
-	return false, err;
+	return nil;
 end
 
+local function kfk_destroy_msgq(kfk, msgq, err)
+    local cf = kfk.cf;
+
+    kfk.msg_cnt = kfk.msg_cnt - msgq.size;
+    if cf.kfk_status then
+        kfk_status_add("msg_cnt", -msgq.size);
+    end
+
+    if err == 0 then
+        if cf.kfk_status then
+            kfk_status_add("succ_msg_cnt", msgq.size);
+        end
+    else
+        ngxlog(ERR, msgq.size, " messages send failure with error:", const.errstr[err]);
+
+        if cf.kfk_status then
+            kfk_status_add("fail_msg_cnt", msgq.size);
+        end
+
+        local head = msgq.head;
+        local kfk_msg = head[msgq.key].next;
+        while kfk_msg ~= head do
+            cf.failed_cb(kfk, kfk_msg);
+            msgq:remove(kfk_msg);
+            kfk_msg = head[msgq.key].next;
+        end
+    end
+    list.init(msgq);
+end
 
 local func = {
-	kfk_new_topic = kfk_new_topic,
-	kfk_topic_find = kfk_topic_find,
-	kfk_new_toppar = kfk_new_toppar,
-	kfk_new_message = kfk_new_message,
-	kfk_message_partitioner = kfk_message_partitioner,
-	kfk_toppar_get = kfk_toppar_get
+    kfk_destroy_msgq = kfk_destroy_msgq,
+
+    kfk_status_add = kfk_status_add,
+    kfk_status_set = kfk_status_set,
+
+	kfk_broker_find_byid = kfk_broker_find_byid, 
+    kfk_broker_ifup = kfk_broker_ifup,
+    
+	kfk_toppar_get = kfk_toppar_get,
+	kfk_topic_find = kfk_topic_find
 }
 
 return func
